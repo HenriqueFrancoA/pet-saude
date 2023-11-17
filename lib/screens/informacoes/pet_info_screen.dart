6c452d92b9396d2fdeeae179be427e0e7fc2bf76
Henrique Franco <henriquefrancoaraujo@gmail.com>
2023-11-17 14:59:24 -0300

    gifs readme

--- lib/screens/informacoes/pet_info_screen.dart
+++ lib/screens/informacoes/pet_info_screen.dart
@@ -284,7 +284,7 @@ class PetInfoScreenState extends State<PetInfoScreen> {
                       ],
                     ),
                   ),
-                  isBannerClosed
+                  !isBannerClosed
                       ? BannerComponent(
                           onBannerClosed: () {
                             setState(() {
